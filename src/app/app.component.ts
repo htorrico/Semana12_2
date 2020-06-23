5355cf9510f1c383836c1403186df00e3f931f45
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-06-23 12:59:56 -0500

    Upload files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UploadService } from './services/upload.service';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  uploadedFiles: Array < File > ;
   title = 'UploadsFiles';
+  constructor(private serviceUpload: UploadService) { }
+  fileChange(element) {
+    console.log('fileChangexx');
+    this.uploadedFiles = element.target.files;
+  }
+  upload() {
+    console.log('uploadxxx');
+    let formData = new FormData();
+    console.log('uploadedFiles:');
+    console.log(this.uploadedFiles);
+    for (var i = 0; i < this.uploadedFiles.length; i++) {
+      console.log('Inicio FOR');
+      // formData.append("file[]",this.uploadedFiles[0]);
+      formData.append("file",
+	    this.uploadedFiles[i],
+      this.uploadedFiles[i].name);
+
+    }
+    console.log('formData:');
+    console.log(formData);
+    this.serviceUpload.uploadFile(formData).subscribe((res)=> {
+      console.log('response received is ', res);
+    });
+  }
 }
